317815f53af129ef20cc7f63c2455abcdeeadeb0
catalinbroinas <catalinbroinas@yahoo.com>
2024-02-05 14:21:36 +0200

    Replace HTML page content with javascript objects

--- src/js/style.js
+++ src/js/style.js
@@ -9,11 +9,11 @@ function Person(firstName, lastName, birthDate, nationality, job, image) {
     this.image = image;
 }
 
-Person.prototype.getFullName = function() {
+Person.prototype.getFullName = function () {
     return `${this.firstName} ${this.lastName}`;
 }
 
-Person.prototype.getAge = function() {
+Person.prototype.getAge = function () {
     const ageDifMs = Date.now() - this.birthDate.getTime();
     const ageDate = new Date(ageDifMs);
     return Math.abs(ageDate.getUTCFullYear() - 1970);
@@ -36,14 +36,99 @@ OutfieldPlayer.prototype = Object.create(Player.prototype);
 OutfieldPlayer.prototype.constructor = OutfieldPlayer;
 
 // Create players
-const alissonBecker = new Player('Alisson', 'Becker', '1992-10-02', 'Brazilian', 'player', 'alisson.webp', 'goalkeeper');
-const alexArnold = new OutfieldPlayer('Alexander', 'Arnold', '1998-10-07', 'English','player', 'arnold.png', 'defender', 'right back');
-const wataruEndo = new OutfieldPlayer('Wataru', 'Endo', '1993-02-09', 'Japanese', 'player', 'endo.png', 'midfielder', 'defensive Midfield');
-const mohamedSalah = new OutfieldPlayer('Mohamed', 'Salah', '1992-06-15', 'Egyptian', 'player', 'salah.png', 'forward', 'right winger');
+const alissonBecker = new Player(
+    'Alisson', 'Becker',
+    '1992-10-02', 'Brazilian',
+    'player', 'alisson.webp',
+    'goalkeeper'
+);
+const alexArnold = new OutfieldPlayer(
+    'Alexander', 'Arnold',
+    '1998-10-07', 'English',
+    'player', 'arnold.png',
+    'defender', 'right back'
+);
+const wataruEndo = new OutfieldPlayer(
+    'Wataru', 'Endo',
+    '1993-02-09', 'Japanese',
+    'player', 'endo.png',
+    'midfielder', 'defensive Midfield'
+);
+const mohamedSalah = new OutfieldPlayer(
+    'Mohamed', 'Salah',
+    '1992-06-15', 'Egyptian',
+    'player', 'salah.png',
+    'forward', 'right winger'
+);
+const vanDijk = new OutfieldPlayer(
+    'Virgil', 'Van Dijk',
+    '1991-07-08', 'Dutch',
+    'player', 'vanDijk.png',
+    'defender', 'centre back'
+);
+const ibrahimaKonate = new OutfieldPlayer(
+    'Ibrahima', 'Konate',
+    '1999-05-25', 'French',
+    'player', 'konate.webp',
+    'defender', 'centre back'
+);
+const andyRobertson = new OutfieldPlayer(
+    'Andy', 'Robertson',
+    '1994-03-11', 'Scottish',
+    'player', 'robertson.png',
+    'defender', 'left back'
+);
+const macAllister = new OutfieldPlayer(
+    'Alexis', 'Mac Allister',
+    '1998-12-24', 'Argentinian',
+    'player', 'macAllister.png',
+    'midfielder', 'central midfield'
+);
+const curtisJones = new OutfieldPlayer(
+    'Curtis', 'Jones',
+    '2001-01-30', 'English',
+    'player', 'jones.png',
+    'midfielder', 'central midfield'
+);
+const luisDiaz = new OutfieldPlayer(
+    'Luis', 'Diaz',
+    '1997-01-13', 'Colombian',
+    'player', 'diaz.png',
+    'forward', 'left winger'
+);
+const darwinNunez = new OutfieldPlayer(
+    'Darwin', 'Nunez',
+    '1999-06-24', 'Uruguayan',
+    'player', 'nunez.png',
+    'forward', 'center forward'
+);
 
-TEAM.push(alissonBecker, alexArnold, wataruEndo, mohamedSalah);
+// Add players
+TEAM.push(
+    alissonBecker,
+    alexArnold,
+    wataruEndo,
+    mohamedSalah,
+    vanDijk,
+    ibrahimaKonate,
+    andyRobertson,
+    macAllister,
+    curtisJones,
+    darwinNunez,
+    luisDiaz
+);
 
-function createPlayerCard({ firstName, lastName, getFullName, birthDate, getAge, nationality, image, role, position }) {
+function createPlayerCard({
+    firstName,
+    lastName,
+    getFullName,
+    birthDate,
+    getAge,
+    nationality,
+    image,
+    role,
+    position
+}) {
     const column = document.createElement('div');
     const card = document.createElement('div');
     const cardImage = document.createElement('img');
@@ -52,7 +137,7 @@ function createPlayerCard({ firstName, lastName, getFullName, birthDate, getAge,
     const cardBody = document.createElement('div');
     const cardAge = document.createElement('p');
     const cardNationality = document.createElement('p');
-    
+
     column.classList.add('col');
     card.classList.add('card', 'border-0', 'h-100', 'shadow-sm', 'rounded-3', 'rounded-top-0');
     cardImage.classList.add('card-img-top', 'object-fit-cover', 'h-100', 'bg-light');
@@ -66,7 +151,7 @@ function createPlayerCard({ firstName, lastName, getFullName, birthDate, getAge,
     cardImage.setAttribute('src', `img/${image}`);
 
     cardFullName.textContent = getFullName.call({ firstName, lastName });
-    cardAge.textContent = getAge.call({birthDate}) + ' years';
+    cardAge.textContent = getAge.call({ birthDate }) + ' years';
     cardNationality.textContent = nationality;
 
     column.appendChild(card);
@@ -76,13 +161,13 @@ function createPlayerCard({ firstName, lastName, getFullName, birthDate, getAge,
     cardHeader.appendChild(cardFullName);
     cardBody.appendChild(cardAge);
 
-    if(position) {
+    if (position) {
         const cardPosition = document.createElement('p');
         cardPosition.classList.add('card-text', 'text-capitalize', 'fw-normal', 'fs-5');
         cardPosition.textContent = position;
         cardBody.appendChild(cardPosition);
     }
-    
+
     cardBody.appendChild(cardNationality);
 
     return column;
@@ -96,7 +181,7 @@ function displayPlayerCards(players) {
 
     players.forEach((player) => {
         const column = createPlayerCard(player);
-        switch(player.role) {
+        switch (player.role) {
             case 'goalkeeper':
                 gkSect.appendChild(column);
                 break;
@@ -106,7 +191,7 @@ function displayPlayerCards(players) {
             case 'midfielder':
                 midSect.appendChild(column);
                 break;
-            case 'forward': 
+            case 'forward':
                 attSect.appendChild(column);
                 break;
         }
@@ -115,4 +200,4 @@ function displayPlayerCards(players) {
 
 window.addEventListener('load', () => {
     displayPlayerCards(TEAM);
-})
+});
